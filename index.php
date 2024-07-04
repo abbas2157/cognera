750275fbc7d2e2f599aa3ea3b7199324b3807329
Mudassar Abbas <mudassar@zoomdigital.ae>
2024-07-04 16:00:45 +0500

    FIle Upload System

--- index.php
+++ index.php
@@ -1,2 +1,41 @@
 <?php
-echo "My Name is Mudassar";
+if($_POST)
+{
+    $file_parts = pathinfo($filename);
+
+    $file_parts['extension'];
+    $cool_extensions = Array('jpg','png','jpeg','dcm','DCM');
+
+    $error = array();
+    $extension = array('jpg','png','jpeg','dcm','DCM');
+    for($i = 0; $i < count($_FILES); $i++) 
+    {
+        $file_name = $_FILES["file-".$i]["name"];
+        $error[] =  $file_name;
+        $file_tmp = $_FILES["file-".$i]["tmp_name"];
+        $ext = pathinfo($file_name,PATHINFO_EXTENSION);
+
+        if(in_array($ext,$extension)) 
+        {
+            if(!file_exists("uploads/".$file_name)) 
+            {
+                move_uploaded_file( $file_tmp = $_FILES["file-".$i]["tmp_name"] , "uploads/".$file_name);
+            }
+            else 
+            {
+                $filename = basename($file_name,$ext);
+                $newFileName = $filename.time().".".$ext;
+                move_uploaded_file($file_tmp = $_FILES["file-".$i]["tmp_name"],"uploads/".$newFileName);
+            }
+        }
+        else 
+        {
+            array_push($error,"$file_name, ");
+        }
+    }
+    return json_encode($error);
+}
+else
+{
+    return "Please Select File.";
+}
---
+++ test.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+<form enctype="multipart/form-data" method="post">
+  <input id="file" name="file[]" type="file"  multiple/>
+  <input class="update" type="submit" />
+</form>
+
+<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+<script>
+
+// Updated part
+jQuery.each(jQuery('#file')[0].files, function(i, file) {
+    data.append('file-'+i, file);
+});
+
+// Full Ajax request
+$(".update").click(function(e) {
+    // Stops the form from reloading
+    e.preventDefault();
+        $.ajax({
+            url: 'index.php',
+            type: 'POST',
+            contentType:false,
+            processData: false,
+            data: function(){
+                var data = new FormData();
+                jQuery.each(jQuery('#file')[0].files, function(i, file) {
+                    data.append('file-'+i, file);
+                });
+                data.append('body' , $('#body').val());
+                data.append('uid', $('#uid').val());
+                return data;
+            }(),
+                success: function(result) {
+                // alert(result);
+                },
+            error: function(xhr, result, errorThrown){
+                alert('Request failed.');
+            }
+        });
+});
+    </script>
+</body>
+</html>
