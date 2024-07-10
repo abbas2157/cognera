74a4e859d4f814a70b77e189262f21cd13310d22
Mudassar Abbas <mudassar@zoomdigital.ae>
2024-07-10 11:06:39 +0500

    Json Response

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-header('Access-Control-Allow-Origin: http://www.example.com');
+header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Credentials: true");
 header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
 
