c2cfc212627100eab186abfc461f7196f31e055b
Mudassar Abbas <mudassar@zoomdigital.ae>
2024-07-10 11:12:55 +0500

    3

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ error_reporting(E_ALL);
 header('Access-Control-Allow-Origin: https://cognera.ai');
 header("Access-Control-Allow-Credentials: true");
 header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
-
+echo json_encode($_POST);
 if($_POST)
 {
     $file_parts = pathinfo($filename);
